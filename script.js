f6f341ee58d3f8c17b49d824badddcfb38624912
Michelle Hokeness <cvaggio@gmail.com>
2021-01-02 09:10:39 -0800

    adding varaibles and function after the button

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Assignment Code
-//Alert employee to create a new, secure password.
-alert("Please create a new, more secure password.");
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -13,39 +10,45 @@ function writePassword() {
 
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-const passwordParts = ["Uppercase Characters", "Lowercase Characters", "Numeric Characters", "Special Characters"];
-const passwordValues = [
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  "abcdefghijklmnopqrstuvwxyz",
-  "0123456789",
-  "!#$%&()<:>\*+-/=?@[|^_;~",
-];
+//Character type arrays
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"]
+
+//variable declarations
+const confirmlength = "";
+const confirmSpecialCharacter;
+const confirmNumericCharacter;
+const confirmUppercase;
+const confirmLowercase;
 
 
+//alert employee to create a new password
+function generatePassword() {
+    alert("Please create a new, more secure password.");
 
-//prompt employee for password length at least 8 characters and no more than 128 characters
+const confirmlength = prompt("How many characters would you like your password to have? Enter a number no less than 8 and no more than 128.");
 
- userLengthprompt = parseInt(prompt("How many characters would you like your password to have? Enter a number no less than 8 and no more than 128."));
-function generate() {
-  
-if (userLengthprompt < 8 ) {
+//create a while loop if employee chooses values outside the parameters
+while(confirmlength <=7 || confirmlength >= 129) {
+  if (confirmlength < 8 ) {
     alert("Your password must have at least 8 characters.");
     return;
 }
-if (userLengthprompt > 128) {
+if (confirmlength > 128) {
     alert("Your password cannot be more than 128 characters.");
     return;
-
 }
 
-//prompt employee for character types
-
- 
-
-
 }
+}
+alert("Your password will have " + confirmlength + "characters.");
+
+
+
+
 
