040a1a68a09f5c642b85890e226e4ba50c038694
Michelle Hokeness <cvaggio@gmail.com>
2021-01-03 09:22:42 -0800

    adding confirm character types to script.js

--- script.js
+++ script.js
@@ -10,32 +10,38 @@ const upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 const lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
-//variable declarations
-const confirmlength = confirm("Choose a password length from 8 characters up to 128 characters");
-const confirmSpecialCharacter = confirm("Would you like special characters in your password?");
-const confirmNumericCharacter = confirm("Would you like to use numbers in your password?");
-const confirmUppercase = confirm("Would you like to have upper case characters in your password?");
-const confirmLowercase = confirm("Would you like to have lower case characters in your password?");
-
 
 //alert employee to create a new password
 function generatePassword() {
     alert("Please create a new, more secure password.");
     
-
 const length = prompt("How many characters would you like your password to have? Enter a number no less than 8 and no more than 128.");
+const numslength = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138];
 
-//create a while loop if employee chooses values outside the parameters
-while(userlength <=7 || userlength >= 129) {
-  if (userlength < 8 ) {
+//create a for loop if employee chooses values outside the parameters
+for (i = 0; i < 128; i++) {
+if (length < 8 || length > 129) {
     alert("Your password must have at least 8 characters and no more than 128 characters.");
     return;
 }
+else {
+alert("Your password will have " + length + " characters.");   
+}
+
+//variable declarations of character types
+const confirmSpecialCharacter = confirm("Would you like special characters in your password?");
+const confirmNumericCharacter = confirm("Would you like to use numbers in your password?");
+const confirmUppercase = confirm("Would you like to have upper case characters in your password?");
+const confirmLowercase = confirm("Would you like to have lower case characters in your password?");
+
+//confirm which character types are to be used in the new password
+//alert at least one must be used if none are chosen
 
 
 }
 }
-alert("Your password will have " + userlength + "characters.");
+         
+
 
 
 
@@ -48,10 +54,4 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
-
-
-
-
 
